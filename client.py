e42a25353ae60112c5673b4baaad12c09ad4d7b6
Dave Masse <dmasse@rudemoose.com>
2011-01-12 15:00:39 -0500

    Tweak to pickle the dictionary before returning it. Add a little documentation.

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
+# Load local settings
 import settings
 
+import pickle
 import pywapi
 import re
 import sys
 
 def main():
+	# Grab the current weather from Google
 	weather = pywapi.get_weather_from_google(settings.LOCATION)
 	
 	weather_dict = {}
@@ -12,15 +15,17 @@ def main():
 	for key in settings.DATA:
 		value = eval('weather' + settings.DATA[key][0])
 		
+		# Run the regex if a value was provided
 		if len(settings.DATA[key]) == 2:
 			matches = re.search(settings.DATA[key][1], value)
 			
 			if matches != None:
 				value = matches.group(0)
 		
+		# Assign the returned value to the new dictionary key
 		weather_dict[key] = value
 	
-	print weather_dict
+	return pickle.dumps(weather_dict)
 
 if __name__ == '__main__':
 	sys.exit(main())
